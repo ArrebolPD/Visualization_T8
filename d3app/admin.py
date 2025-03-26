f8cfaccf3253ae78176a8a682d20e1a12868bdc8
ArrebolPD <phuongdonghoang2@gmail.com>
2025-03-26 18:51:31 +0700

    django_project

---
+++ d3app/admin.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import Customer, ProductGroup, Product, Order, OrderDetail
+
+admin.site.register(Customer)
+admin.site.register(ProductGroup)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
